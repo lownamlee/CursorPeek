b3a14c7b87ad5abe14ef0f831d2b2f628b6e7565
Low Nam Lee <112510782+lownamlee@users.noreply.github.com>
2026-07-27 11:57:08 +0800

    build: follow authenticated NSIS mirror redirects

--- tools/Get-Nsis.ps1
+++ tools/Get-Nsis.ps1
@@ -13,10 +13,13 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $nsisVersion = '3.12'
 $archiveSha256 = '56581F90DB321581C5381193D796FFFCF2D24B2F8FED2160A6C6A3BAA67F2C4F'
-$downloadUri = (
-    'https://downloads.sourceforge.net/project/nsis/NSIS%203/{0}/nsis-{0}.zip' -f
+$downloadPageUri = (
+    'https://sourceforge.net/projects/nsis/files/NSIS%203/{0}/nsis-{0}.zip/download' -f
     $nsisVersion
 )
+$mirrorHost = 'downloads.sourceforge.net'
+$mirrorPath = "/project/nsis/NSIS%203/$nsisVersion/nsis-$nsisVersion.zip"
+$maximumRedirectPageBytes = 512KB
 $resolvedDestination = [System.IO.Path]::GetFullPath($DestinationDirectory)
 $archiveDirectory = Join-Path $resolvedDestination 'nsis-download'
 $archivePath = Join-Path $archiveDirectory "nsis-$nsisVersion.zip"
@@ -57,6 +60,99 @@ function Get-Sha256Stream {
     }
     finally {
         $algorithm.Dispose()
+    }
+}
+
+function Get-SourceForgeMirrorUri {
+    param([Parameter(Mandatory = $true)][string] $LiteralPath)
+
+    $file = [System.IO.FileInfo] $LiteralPath
+    if ($file.Length -gt $maximumRedirectPageBytes) {
+        throw 'The unexpected NSIS response is too large to be a mirror redirect page.'
+    }
+
+    $html = [System.IO.File]::ReadAllText($LiteralPath)
+    if (-not $html.TrimStart().StartsWith(
+        '<!doctype html',
+        [System.StringComparison]::OrdinalIgnoreCase
+    )) {
+        throw 'The unexpected NSIS response is not a SourceForge redirect page.'
+    }
+
+    $redirects = [System.Collections.Generic.List[string]]::new()
+    foreach ($metaMatch in [regex]::Matches($html, '(?is)<meta\b[^>]*>')) {
+        $tag = $metaMatch.Value
+        $httpEquiv = [regex]::Match(
+            $tag,
+            '(?is)\bhttp-equiv\s*=\s*(["''])(?<value>.*?)\1'
+        )
+        if (-not $httpEquiv.Success -or
+            $httpEquiv.Groups['value'].Value -cne 'refresh') {
+            continue
+        }
+
+        $content = [regex]::Match(
+            $tag,
+            '(?is)\bcontent\s*=\s*(["''])(?<value>.*?)\1'
+        )
+        if (-not $content.Success) {
+            continue
+        }
+        $target = [regex]::Match(
+            $content.Groups['value'].Value,
+            '(?is)^\s*\d+\s*;\s*url=(?<url>.+?)\s*$'
+        )
+        if ($target.Success) {
+            $redirects.Add(
+                [System.Net.WebUtility]::HtmlDecode(
+                    $target.Groups['url'].Value
+                )
+            )
+        }
+    }
+
+    if ($redirects.Count -ne 1) {
+        throw 'The SourceForge response did not contain exactly one mirror redirect.'
+    }
+
+    $uri = [System.Uri] $redirects[0]
+    if ($uri.Scheme -cne 'https' -or
+        $uri.Host -cne $mirrorHost -or
+        -not $uri.IsDefaultPort -or
+        $uri.UserInfo.Length -ne 0 -or
+        $uri.Fragment.Length -ne 0 -or
+        $uri.AbsolutePath -cne $mirrorPath) {
+        throw "SourceForge returned an unexpected NSIS mirror URI '$uri'."
+    }
+    return $uri
+}
+
+function Save-AuthenticatedNsisArchive {
+    param([Parameter(Mandatory = $true)][string] $LiteralPath)
+
+    $initialDownload = "$LiteralPath.initial"
+    try {
+        Invoke-WebRequest -Uri $downloadPageUri -OutFile $initialDownload
+        if ((Get-Sha256Hex $initialDownload) -ceq $archiveSha256) {
+            [System.IO.File]::Move($initialDownload, $LiteralPath)
+            return
+        }
+
+        $mirrorUri = Get-SourceForgeMirrorUri $initialDownload
+        Invoke-WebRequest -Uri $mirrorUri -OutFile $LiteralPath
+        $actualHash = Get-Sha256Hex $LiteralPath
+        if ($actualHash -cne $archiveSha256) {
+            throw "Downloaded NSIS has SHA-256 $actualHash; expected $archiveSha256."
+        }
+    }
+    finally {
+        if ([System.IO.File]::Exists($initialDownload)) {
+            [System.IO.File]::Delete($initialDownload)
+        }
+        if ([System.IO.File]::Exists($LiteralPath) -and
+            (Get-Sha256Hex $LiteralPath) -cne $archiveSha256) {
+            [System.IO.File]::Delete($LiteralPath)
+        }
     }
 }
 
@@ -154,11 +250,7 @@ if ([System.IO.File]::Exists($archivePath)) {
 else {
     $temporaryDownload = "$archivePath.$([System.Guid]::NewGuid().ToString('N')).download"
     try {
-        Invoke-WebRequest -Uri $downloadUri -OutFile $temporaryDownload
-        $actualHash = Get-Sha256Hex $temporaryDownload
-        if ($actualHash -cne $archiveSha256) {
-            throw "Downloaded NSIS has SHA-256 $actualHash; expected $archiveSha256."
-        }
+        Save-AuthenticatedNsisArchive $temporaryDownload
         [System.IO.File]::Move($temporaryDownload, $archivePath)
     }
     finally {
